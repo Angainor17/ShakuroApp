c3a733819f82e0a5bd7321fdbe922d0347d5d70e
angai <angainor17@gmail.com>
2019-07-10 20:08:23 +0300

    Initial commit

---
+++ app/src/main/java/com/voronin/shakuro/contributorDetail/ContributorDetailFragment.kt
@@ -0,0 +1,9 @@
+package com.voronin.shakuro.contributorDetail
+
+import androidx.fragment.app.Fragment
+
+/**
+ * TODO
+ */
+class ContributorDetailFragment : Fragment() {
+}
---
+++ app/src/main/java/com/voronin/shakuro/contributorsList/ContributorFragment.kt
@@ -0,0 +1,9 @@
+package com.voronin.shakuro.contributorsList
+
+import androidx.fragment.app.Fragment
+
+/**
+ * TODO
+ */
+class ContributorFragment : Fragment() {
+}
